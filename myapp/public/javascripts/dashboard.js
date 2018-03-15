b0c0b08b6a504ab80ca68077d63adf7ec497a359
pathiratk <pathirat.kos@gmail.com>
2018-03-15 00:27:07 -0700

    remove modal

--- myapp/public/javascripts/dashboard.js
+++ myapp/public/javascripts/dashboard.js
@@ -1,9 +1,10 @@
 // Get the modal
 var modal = document.getElementById('myModal');
-
+// window.onload = function () {
+//     modal.style.display = "none";
+// }
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-console.log(btn);
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
